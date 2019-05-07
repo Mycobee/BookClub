252d594708d72db72c6d034473b42180428040b4
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-05-07 12:24:27 -0600

    Passing tests for NavBar. Closes card-#40
    Co-authored_by: Rob Stringer <41843577+Mycobee@users.noreply.github.com>

--- bookclub/config/routes.rb
+++ bookclub/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/books', to: 'books#index'
+  get '/home', to: 'home#index', as: 'home'
+  resources :books, only: [:index]
 end
