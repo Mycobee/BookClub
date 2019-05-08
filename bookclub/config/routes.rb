2dbd4e413ffabed536f62147f870ffa7be5a3ea6
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-05-08 16:12:13 -0600

    Adds card-#61, passing tests for author book show page.
    Co-authored_by: Rob Stringer <41843577+Mycobee@users.noreply.github.com>

--- bookclub/config/routes.rb
+++ bookclub/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  resources :home, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/home', to: 'home#index', as: 'home'
+  # get '/home', to: 'home#index', as: 'home'
   resources :books, only: [:index]
+
+  resources :authors, only: [:show]
 end
--- bookclub/spec/features/nav_bar/navigation_index_spec.rb
+++ bookclub/spec/features/nav_bar/navigation_index_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe 'navigation bar', type: :feature do
 
     click_link "Home"
 
-    expect(current_path).to eq(home_path)
+    expect(current_path).to eq(home_index_path)
   end
 end
