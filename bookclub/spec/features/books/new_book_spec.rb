57f24ea179d246995101fee3a5b3ef771bcd9fc3
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-05-09 10:45:58 -0600

    adds titlized feature test.
    Co-authored_by: Rob Stringer <41843577+Mycobee@users.noreply.github.com>

--- bookclub/spec/features/books/new_book_spec.rb
+++ bookclub/spec/features/books/new_book_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'as a user on the book index page', type: :feature do
 
       expect(current_path).to eq(new_book_path)
 
-      fill_in 'book[title]', with: "Cool Book Name"
+      fill_in 'book[title]', with: "cool book name"
       fill_in 'book[pages]', with: 100
       fill_in 'book[pub_year]', with: 1982
       fill_in 'book[thumb_url]', with: '/path/to/image.jpg'
@@ -18,6 +18,8 @@ RSpec.describe 'as a user on the book index page', type: :feature do
       expect(current_path).to eq(books_path)
 
       @new_book = Book.last
+      expect(@new_book.name).to eq("Cool Book Name")
+
       within("#book-#{@new_book.id}") do
         expect(page).to have_content(@new_book.title)
       end
