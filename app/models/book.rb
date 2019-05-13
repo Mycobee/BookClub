8c5cec77cd1ad7d60a23858d71132790d0fa63aa
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-05-13 15:57:13 -0600

    Added ascending_pages, ascending_review_count, and corresponding descending methods.
    Co-authored_by: Rob Stringer <41843577+Mycobee@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,30 @@ class Book < ApplicationRecord
   validates :pub_year, presence: true, numericality: true, inclusion: { in: 1455..2025}
 
   before_save  {self.title = self.title.titleize}
+
+  def total_reviews
+    self.reviews.count
+  end
+
+  def average_rating
+    self.reviews.average(:score)
+  end
+
+  def self.ascending_pages
+      Book.order(:pages)
+  end
+
+  def self.descending_pages
+    Book.order(pages: :desc)
+  end
+
+  def self.ascending_review_count
+    book_ids = Review.joins(:book).select('book_id, count(*)').group(:book_id).order(count: :asc).pluck(:book_id)
+    Book.find(book_ids)
+  end
+
+  def self.descending_review_count
+    book_ids = Review.joins(:book).select('book_id, count(*)').group(:book_id).order(count: :desc).pluck(:book_id)
+    Book.find(book_ids)
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -21,13 +21,67 @@ RSpec.describe Book, type: :model do
   # it {should validate_uniqueness_of(:title)}
   end
 
-  # describe 'instance methods' do
-  #
-  # end
-  #
-  # describe 'class methods' do
-  #
-  # end
+  describe 'instance methods' do
+    it '#total_reviews' do
+      book_1 = Book.create!(title: "The Hobbit", pub_year: 1937, pages: 310, thumb_url: "this_is_a_test_url")
+      author_1 = book_1.authors.create!(name: "J.R.R. Tolkien")
 
+      review_1 = book_1.reviews.create!(user: "Mills", heading: "Couldn't dig it.", full_review: "Not my jam, too hard to read. Not enough pictures", score: 2)
+      review_2 = book_1.reviews.create!(user: "Rob", heading: "BEST BOOK EVER", full_review: "OMG, it was like, THE BEST BOOK. FOREVER. LITERALLY. EVER. LITERALLY.", score: 5)
 
+      expect(book_1.total_reviews).to eq(2)
+    end
+
+    it '#average_rating' do
+      book_1 = Book.create!(title: "The Hobbit", pub_year: 1937, pages: 310, thumb_url: "this_is_a_test_url")
+      author_1 = book_1.authors.create!(name: "J.R.R. Tolkien")
+
+      review_1 = book_1.reviews.create!(user: "Mills", heading: "Couldn't dig it.", full_review: "Not my jam, too hard to read. Not enough pictures", score: 2)
+      review_2 = book_1.reviews.create!(user: "Rob", heading: "BEST BOOK EVER", full_review: "OMG, it was like, THE BEST BOOK. FOREVER. LITERALLY. EVER. LITERALLY.", score: 4)
+
+      expect(book_1.average_rating).to eq(3)
+    end
+  end
+
+  describe 'class methods' do
+    it '.ascending_pages' do
+      book_1 = Book.create!(title: "The Hobbit", pub_year: 1937, pages: 500, thumb_url: "this_is_a_test_url")
+      book_2 = Book.create!(title: "Not The Hobbit", pub_year: 1937, pages: 310, thumb_url: "this_is_a_test_url")
+      book_3 = Book.create!(title: "Maybe The Hobbit", pub_year: 1937, pages: 110, thumb_url: "this_is_a_test_url")
+
+      expect(Book.ascending_pages).to eq([book_3, book_2, book_1])
+    end
+
+    it '.decending_pages' do
+      book_1 = Book.create!(title: "The Hobbit", pub_year: 1937, pages: 500, thumb_url: "this_is_a_test_url")
+      book_2 = Book.create!(title: "Not The Hobbit", pub_year: 1937, pages: 310, thumb_url: "this_is_a_test_url")
+      book_3 = Book.create!(title: "Maybe The Hobbit", pub_year: 1937, pages: 110, thumb_url: "this_is_a_test_url")
+
+      expect(Book.descending_pages).to eq([book_1, book_2, book_3])
+    end
+
+    it '.ascending_review_count' do
+      book_1 = Book.create!(title: "The Hobbit", pub_year: 1937, pages: 500, thumb_url: "this_is_a_test_url")
+      book_2 = Book.create!(title: "Not The Hobbit", pub_year: 1937, pages: 310, thumb_url: "this_is_a_test_url")
+
+      review_1 = book_1.reviews.create!(user: "Mills", heading: "Couldn't dig it.", full_review: "Not my jam, too hard to read. Not enough pictures", score: 2)
+      review_2 = book_1.reviews.create!(user: "Rob", heading: "BEST BOOK EVER", full_review: "OMG, it was like, THE BEST BOOK. FOREVER. LITERALLY. EVER. LITERALLY.", score: 4)
+
+      review_3 = book_2.reviews.create!(user: "Joe", heading: "Eh", full_review: "Great book", score: 3)
+
+      expect(Book.ascending_review_count).to eq([book_2, book_1])
+    end
+
+    it '.descending_review_count' do
+        book_1 = Book.create!(title: "The Hobbit", pub_year: 1937, pages: 500, thumb_url: "this_is_a_test_url")
+        book_2 = Book.create!(title: "Not The Hobbit", pub_year: 1937, pages: 310, thumb_url: "this_is_a_test_url")
+
+        review_1 = book_1.reviews.create!(user: "Mills", heading: "Couldn't dig it.", full_review: "Not my jam, too hard to read. Not enough pictures", score: 2)
+        review_2 = book_1.reviews.create!(user: "Rob", heading: "BEST BOOK EVER", full_review: "OMG, it was like, THE BEST BOOK. FOREVER. LITERALLY. EVER. LITERALLY.", score: 4)
+
+        review_3 = book_2.reviews.create!(user: "Joe", heading: "Eh", full_review: "Great book", score: 3)
+
+        expect(Book.descending_review_count).to eq([book_1, book_2])
+    end
+  end
 end
