8de3824c9354f1484cd5edd6fac30d6c7628399d
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-05-14 14:22:34 -0600

    Adds test and functionality for review link.
    Co-authored_by: Rob Stringer <41843577+Mycobee@users.noreply.github.com>

--- spec/features/reviews/user_links_spec.rb
+++ spec/features/reviews/user_links_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'As a visitor anywhere on the site' do
       @book_1 = Book.create!(title: "The Hobbit", pub_year: 1937, pages: 310, thumb_url: "this_is_a_test_url")
       @author_1 = @book_1.authors.create!(name: "J.R.R. Tolkien")
       @review_1 = @book_1.reviews.create(user: "Mills", heading: "Awesome!", full_review: "Yay", score: 5)
-      @review_2 = @book_1.reviews.create(user: "Bob", heading: "Not good.", full_review: "Boo", score: 1)
+      # @review_2 = @book_1.reviews.create(user: "Bob", heading: "Not good.", full_review: "Boo", score: 1)
     end
 
     it 'is a link I can click to the user show page' do
@@ -16,6 +16,11 @@ RSpec.describe 'As a visitor anywhere on the site' do
 
       expect(current_path).to eq(reviews_path)
 
+      visit book_path(@book_1)
+
+      click_link(@review_1.user)
+
+      expect(current_path).to eq(reviews_path)
     end
   end
 end
